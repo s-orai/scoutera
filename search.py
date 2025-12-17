e7e4c1bc9d00758050d17ca8f02ecc5b4d3391c0
s-orai <rai1125.30706@gmail.com>
2025-12-17 15:12:01 +0900

    画面追加

--- search.py
+++ search.py
@@ -6,58 +6,138 @@ from services import logic
 def show_search_console():
 
   st.set_page_config(page_title="Scout Automation", layout="wide")
-
   st.title('Scout Automation')
 
-  pdfs = st.file_uploader('候補者PDFアップロード', type=['pdf'], accept_multiple_files=True)
+  tab1, tab2 = st.tabs(["候補者ピックアップ", "プロンプト作成"], default="候補者ピックアップ")
 
-  job_pdf = st.file_uploader('求人票アップロード', type=['pdf'], accept_multiple_files=False)
+  with tab1:
+    pdfs = st.file_uploader('候補者PDFアップロード', type=['pdf'], accept_multiple_files=True)
 
-  judge_condition = st.text_area('A/B/C判定の基準', placeholder='判定の基準を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
+    job_pdf = st.file_uploader('求人票アップロード', type=['pdf'], accept_multiple_files=False)
 
-
-  required_condition = st.text_area('必須要件', placeholder='必須要件を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
-
-  welcome_condition = st.text_area('歓迎要件', placeholder='歓迎要件を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
-
-  temperature = st.number_input('temperature', min_value = 0.0, max_value = 2.10, value = 0.5, step = 0.1, width=300)
+    judge_condition = st.text_area('A/B/C判定の基準', placeholder='判定の基準を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
 
 
-  if st.button('開始'):
-    with st.spinner('処理中です.....'):
-      # 入力チェック：どちらか欠けている場合は処理しない
-      if not pdfs or job_pdf is None:
-        st.error("候補者PDFと求人票PDFの両方をアップロードしてください。")
-        return
+    required_condition = st.text_area('必須要件', placeholder='必須要件を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
 
-      temp_file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
-      for uploaded_file in pdfs:
+    welcome_condition = st.text_area('歓迎要件', placeholder='歓迎要件を入力してください、入力内容がそのままプロンプトに反映されるので、箇条書きが好ましいです', height='content')
+
+    temperature = st.number_input('temperature', min_value = 0.0, max_value = 2.10, value = 0.5, step = 0.1, width=300)
+
+
+    if st.button('開始'):
+      with st.spinner('処理中です.....'):
+        # 入力チェック：どちらか欠けている場合は処理しない
+        if not pdfs or job_pdf is None:
+          st.error("候補者PDFと求人票PDFの両方をアップロードしてください。")
+          return
+
+        temp_file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
+        for uploaded_file in pdfs:
+          with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+            tmp_file.write(uploaded_file.getvalue()) # アップロードされたファイルの内容を書き込む
+            temp_file_info.append((tmp_file.name, uploaded_file.name))
+            print(f"ファイル書き込み完了 ファイルパス: {tmp_file.name}")
+
+        temp_job_file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_job_file:
+          tmp_job_file.write(job_pdf.getvalue()) # アップロードされたファイルの内容を書き込む
+          temp_job_file_info.append((tmp_job_file.name, job_pdf.name))
+          print(f"ファイル書き込み完了 ファイルパス: {tmp_job_file.name}")
+
+        try:
+          spreadsheet_url = logic.main(judge_condition, required_condition, welcome_condition, temp_file_info, temp_job_file_info, temperature)
+          st.write(f"作成したシート：{spreadsheet_url}")
+        finally:
+          # 4. ローカルの一時ファイルを削除
+          for tmp_pdf_path, _ in temp_file_info:
+            try:
+              os.remove(tmp_pdf_path)
+            except FileNotFoundError:
+              # 何らかの理由で既に削除されている場合はスキップ
+              pass
+
+          for tmp_job_pdf_path, _ in temp_job_file_info:
+            try:
+              os.remove(tmp_job_pdf_path)
+            except FileNotFoundError:
+              pass
+
+  with tab2:
+    pdfs_A = st.file_uploader('A評価された候補者PDFアップロード', type=['pdf'], accept_multiple_files=True)
+
+    pdfs_B = st.file_uploader('B評価された候補者PDFアップロード', type=['pdf'], accept_multiple_files=True)
+
+    pdfs_C = st.file_uploader('C評価された候補者PDFアップロード', type=['pdf'], accept_multiple_files=True)
+
+    job_pdf = st.file_uploader('求人票PDFアップロード', type=['pdf'], accept_multiple_files=False)
+
+    comment_B = st.text_area('B評価コメント', placeholder='B評価のコメントを入力してください', height='content')
+
+    comment_C = st.text_area('C評価コメント', placeholder='C評価のコメントを入力してください', height='content')
+
+    temperature_cp = st.number_input('temperature_cp', min_value = 0.0, max_value = 2.10, value = 0.5, step = 0.1, width=300)
+
+    if st.button('プロンプト作成開始'):
+      with st.spinner('処理中です.....'):
+        # 入力チェック：どちらか欠けている場合は処理しない
+        if (pdfs_A or pdfs_B or pdfs_C or job_pdf) is None:
+          st.error("評価PDF A, B, Cと求人票PDFの全てをアップロードしてください。")
+          return
+
+        if not comment_B or comment_C is None:
+          st.error("評価コメントを入力してください。")
+          return
+
+        pdfs_A_info = uploade_file(pdfs_A)
+        pdfs_B_info = uploade_file(pdfs_B)
+        pdfs_C_info = uploade_file(pdfs_C)
+
+        job_file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-          tmp_file.write(uploaded_file.getvalue()) # アップロードされたファイルの内容を書き込む
-          temp_file_info.append((tmp_file.name, uploaded_file.name))
+          tmp_file.write(job_pdf.getvalue()) # アップロードされたファイルの内容を書き込む
+          job_file_info.append((tmp_file.name, job_pdf.name))
           print(f"ファイル書き込み完了 ファイルパス: {tmp_file.name}")
 
-      temp_job_file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
-      with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_job_file:
-        tmp_job_file.write(job_pdf.getvalue()) # アップロードされたファイルの内容を書き込む
-        temp_job_file_info.append((tmp_job_file.name, job_pdf.name))
-        print(f"ファイル書き込み完了 ファイルパス: {tmp_job_file.name}")
+        try:
+          spreadsheet_url = logic.create_prompt(pdfs_A_info, pdfs_B_info, pdfs_C_info, comment_B, comment_C, job_file_info, temperature_cp)
+          st.write(f"作成したシート：{spreadsheet_url}")
+        finally:
+          # 4. ローカルの一時ファイルを削除
+          for tmp_pdfA_path, _ in pdfs_A_info:
+            try:
+              os.remove(tmp_pdfA_path)
+            except FileNotFoundError:
+              # 何らかの理由で既に削除されている場合はスキップ
+              pass
 
-      try:
-        spreadsheet_url = logic.main(judge_condition, required_condition, welcome_condition, temp_file_info, temp_job_file_info, temperature)
-        st.write(f"作成したシート：{spreadsheet_url}")
-      finally:
-        # 4. ローカルの一時ファイルを削除
-        for tmp_pdf_path, _ in temp_file_info:
-          try:
-            os.remove(tmp_pdf_path)
-          except FileNotFoundError:
-            # 何らかの理由で既に削除されている場合はスキップ
-            pass
+          for tmp_pdfB_path, _ in pdfs_B_info:
+            try:
+              os.remove(tmp_pdfB_path)
+            except FileNotFoundError:
+              # 何らかの理由で既に削除されている場合はスキップ
+              pass
 
-        for tmp_job_pdf_path, _ in temp_job_file_info:
-          try:
-            os.remove(tmp_job_pdf_path)
-          except FileNotFoundError:
-            pass
+          for tmp_pdfC_path, _ in pdfs_C_info:
+            try:
+              os.remove(tmp_pdfC_path)
+            except FileNotFoundError:
+              # 何らかの理由で既に削除されている場合はスキップ
+              pass
 
+          for tmp_job_pdf_path, _ in job_file_info:
+            try:
+              os.remove(tmp_job_pdf_path)
+            except FileNotFoundError:
+              # 何らかの理由で既に削除されている場合はスキップ
+              pass
+
+def uploade_file(pdfs):
+  file_info = [] # [(一時パス, オリジナルファイル名), ...] を格納
+  for uploaded_file in pdfs:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+      tmp_file.write(uploaded_file.getvalue()) # アップロードされたファイルの内容を書き込む
+      file_info.append((tmp_file.name, uploaded_file.name))
+      print(f"ファイル書き込み完了 ファイルパス: {tmp_file.name}")
+  
+  return file_info
